2904540da65ad44ac61340b401273300936937ae
Nicolai Bonderup <nvbonderup@gmail.com>
2017-09-14 11:44:34 +0200

    Optimization
    Moved file initialization and environmental variable assignment into its own function, preventing extraneous expensive I/O calls for every spider. Reduced time to completion by approximately 10 minutes.

--- boliga/boliga/spiders/sales.py
+++ boliga/boliga/spiders/sales.py
@@ -13,6 +13,12 @@ class SalesSpider(scrapy.Spider):
     base_url = 'http://138.197.184.35/boliga/'
     
     
+    def start_requests(self):
+        self.setup_file()
+        
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
     def parse(self, response):
 
         pages_to_crawl = response.xpath('//a/@href').extract()
@@ -59,26 +65,8 @@ class SalesSpider(scrapy.Spider):
         
     def save_rows(self, data_rows):
         
-        # Checks the operating system in order to determine newline rules.
-        if platform.system() == 'Windows':
-            newline = ''
-        else:
-            newline = None
-        
-        # Checks if the .csv file exists.
-        # If it doesn't, create the file, and add a header. If it does, just append to the file.
-        if os.path.exists("boliga_prices.csv"):
-            append_write = 'a'
-        else:
-            append_write = 'w'
-            title_row = ("address","zip_code","price","sell_date","sell_type","price_per_sq_m","no_rooms","housing_type","size_in_sq_m","year_of_construction","price_change_in_pct")
-            data_rows = [title_row] + data_rows
-
-        # Output the .csv file in the working directory.
-        csv_output_path = os.path.join(os.getcwd(), "boliga_prices.csv")
-
         # Open/Create the file, then write the tuples to it.
-        with open(csv_output_path, append_write, newline=newline, encoding='utf-8') as f:
+        with open(self.csv_output_path, 'a', newline=self.newline, encoding='utf-8') as f:
             output_writer = csv.writer(f)
             for row in data_rows:
                 output_writer.writerow(row)
@@ -86,3 +74,23 @@ class SalesSpider(scrapy.Spider):
         # Updates the progress bar
         self.pbar.update()
             
+            
+                
+    def setup_file(self): 
+        self.csv_output_path = os.path.join(os.getcwd(), "boliga_prices.csv")
+    
+        # Checks the operating system in order to determine newline rules.
+        if platform.system() == 'Windows':
+            self.newline = ''
+        else:
+            self.newline = None
+    
+        if not os.path.exists("boliga_prices.csv"):
+            # Output the .csv file in the working directory.
+            title_row = ("address","zip_code","price","sell_date","sell_type","price_per_sq_m","no_rooms","housing_type","size_in_sq_m","year_of_construction","price_change_in_pct")
+            with open(self.csv_output_path, 'w', newline=self.newline, encoding='utf-8') as f:
+                output_writer = csv.writer(f)
+                output_writer.writerow(title_row)
+        
+        
+        
