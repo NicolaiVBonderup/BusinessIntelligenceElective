49d4047bdce7b930915e26aab3e1edcd997ee3ba
Nicolai Bonderup <nvbonderup@gmail.com>
2017-09-16 20:33:42 +0200

    Optimization, refactoring, directory creation
    Refactored several things, such as .csv file creation, into their own methods for code reuse.
    Added a dict lookup method such that we can look up if a zipcode file exists already by checking a dict instead of directory lookup, since I/O is always expensive.
    Moved the order of the 'with open' command in 'save_rows_separate' and refactored the method such that we can determine the correct .CSV file before the for loop. This in particular has *greatly* increased performance, almost five-fold.

--- boliga/boliga/spiders/sales.py
+++ boliga/boliga/spiders/sales.py
@@ -12,33 +12,41 @@ class SalesSpider(scrapy.Spider):
     start_urls = ['http://138.197.184.35/boliga/']
     base_url = 'http://138.197.184.35/boliga/'
     
+    
+    # Add checks for if zipcode follows zipcode rules, lots of nonconforming values that need to be removed
+    
     def __init__(self, zip_split='', *args, **kwargs):
         super(SalesSpider, self).__init__(*args, **kwargs)
         # Argument to determine if we're making one big CSV file, or CSV files separated by zip code.
         self.zip_split = zip_split
+        
+        # Checks the operating system in order to determine newline rules.
+        if platform.system() == 'Windows':
+            self.newline = ''
+        else:
+            self.newline = None
         
         if (zip_split is ''):
             print("zip_split argument not applied, saving data into one file.")
             self.zip_split = 'n'
         elif (zip_split is 'y'):
             print("Saving data into zipcode-separated CSV files. This will take a while.")
+            # Using a dict as a lookup table for zipcodes
+            # Dict lookup is O(1), list lookup is O(n)
+            self.zip_dict = {'key':'value'}
+            self.setup_file_split_zips()
         elif (zip_split is 'n'):
             print("Saving data into single CSV file.")
+            self.setup_file()
         else:
             print("Invalid zip_split parameter.")
             quit()
+            
+            
+        
     
     def start_requests(self):
-    
-        # Checks the operating system in order to determine newline rules.
-        if platform.system() == 'Windows':
-            self.newline = ''
-        else:
-            self.newline = None
             
-        if(self.zip_split is 'n'):
-            self.setup_file()
-        
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
@@ -91,24 +99,25 @@ class SalesSpider(scrapy.Spider):
         
         
         if (self.zip_split is 'y'):
-            self.save_rows_separate(data_rows)
+            self.save_rows_separate(data_rows,zip_values[0])
         else:
             self.save_rows(data_rows)
         
         
-    def save_rows_separate(self, data_rows):
+    def save_rows_separate(self, data_rows, zipcode):
+    
+        csv_output_path = 'sales/' + zipcode + '.csv'
+        is_zip_file_created = self.dict_zip_lookup(zipcode)
     
         # Open/Create the file, then write the tuples to it.
-        for row in data_rows:
-            zipcode = row[1]
-            csv_output_path = 'sales/' + zipcode[:4] + '.csv'
-            if not os.path.exists(csv_output_path):
-                title_row = ("address","zip_code","price","sell_date","sell_type","price_per_sq_m","no_rooms","housing_type","size_in_sq_m","year_of_construction","price_change_in_pct")
-                with open(csv_output_path, 'w', newline=self.newline, encoding='utf-8') as f:
-                    output_writer = csv.writer(f)
-                    output_writer.writerow(title_row)
-            else:
-                with open(csv_output_path, 'a', newline=self.newline, encoding='utf-8') as f:
+        with open(csv_output_path, 'a', newline=self.newline, encoding='utf-8') as f:
+            for row in data_rows:
+                
+                # Using a dict lookup instead of directory lookup to avoid I/O operations
+                if not is_zip_file_created:
+                    self.create_csv_with_header(csv_output_path)
+                else:
+                
                     output_writer = csv.writer(f)
                     output_writer.writerow(row)
                 
@@ -128,16 +137,30 @@ class SalesSpider(scrapy.Spider):
         self.pbar.update()
             
             
+            
+    def dict_zip_lookup(self, zipcode):
+        # If zipcode is in the dict, we have a file for it already.
+        if (zipcode in self.zip_dict):
+            return True
+        # If zipcode is not in dict, we need to create a file for it.
+        else:
+            self.zip_dict[zipcode] = zipcode
+            return False
                 
     def setup_file(self): 
         self.csv_output_path = os.path.join(os.getcwd(), "boliga_prices.csv")
     
         if not os.path.exists("boliga_prices.csv"):
             # Output the .csv file in the working directory.
-            title_row = ("address","zip_code","price","sell_date","sell_type","price_per_sq_m","no_rooms","housing_type","size_in_sq_m","year_of_construction","price_change_in_pct")
-            with open(self.csv_output_path, 'w', newline=self.newline, encoding='utf-8') as f:
-                output_writer = csv.writer(f)
-                output_writer.writerow(title_row)
-        
-        
+            self.create_csv_with_header(self.csv_output_path)
+                
+    def setup_file_split_zips(self):
+        if not os.path.exists("sales"):
+            os.makedirs("sales")
+     
+    def create_csv_with_header(self, output_path):
+        title_row = ("address","zip_code","price","sell_date","sell_type","price_per_sq_m","no_rooms","housing_type","size_in_sq_m","year_of_construction","price_change_in_pct")
+        with open(output_path, 'w', newline=self.newline, encoding='utf-8') as f:
+            output_writer = csv.writer(f)
+            output_writer.writerow(title_row)
         
